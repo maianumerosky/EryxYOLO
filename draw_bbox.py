b085e377cff4b9b1b6f1d014bc90033484245f0a
carlo <cgferrari@eryx.co>
2023-05-18 20:05:46 +0000

    first real commit

---
+++ draw_bbox.py
@@ -0,0 +1,69 @@
+import torchvision
+import matplotlib.pyplot as plt
+import torch
+
+img_size = 448
+
+class_dict ={0:"fondo",
+1:"metal_1",
+2:"metal_2",
+3:"metal_alargado",
+4:"plastico_der",
+5:"plastico_izq",
+6:"sonrisa_chica",
+7:"sonrisa_grande",
+8:"sticker"}
+
+def get_object_class(class_tensor):
+    """
+    Given a tensor with the class probabilities, returns a string with the name of the most likely class
+    """
+    index = torch.argmax(class_tensor).item()
+    return class_dict[index]
+
+
+def torch_to_pil(img):
+  return torchvision.transforms.ToPILImage()(img).convert('RGB')
+
+def draw_img_and_bbox_from_true_labels(img, labels):
+    """
+    Takes img and labels from the dataset class and draws a PIL image with the bounding boxes for the labeled data.
+    """
+    fig, ax = plt.subplots()
+
+    ax.imshow(torch_to_pil(img))
+    ax.axis("off")
+
+    #ahora las bounding box
+    img_size = 448
+    grid_labels = labels[3,3,:]
+
+    for i in range(7):
+        for j in range(7):
+            grid_labels = labels[i,j,:]
+            draw_grid_bbox(grid_labels, ax)
+            
+            
+def draw_grid_bbox(grid_labels, ax):
+    """
+    Draws the true bounding box for a single grid cell. Takes the grid labels in the format 2B + C and an axes object. Draws the box and writes the class and confidence.
+    """
+    x = grid_labels[0] * img_size
+    y = grid_labels[1] * img_size
+    w = grid_labels[2] * img_size
+    h = grid_labels[3] * img_size
+
+    confidence = grid_labels[4]
+    object_class = grid_labels[10:]
+    x0 = x - w/2
+    x1 = x + w/2
+    y0 = y - h/2
+    y1 = y + h/2
+
+    object_class_name = get_object_class(object_class)
+    if confidence > 0.5:
+        ax.plot([x0,x0], [y0, y1], c = "y")
+        ax.plot([x1,x1], [y0, y1], c = "y")
+        ax.plot([x0,x1], [y0, y0], c = "y")
+        ax.plot([x0,x1], [y1, y1], c = "y")
+        ax.text(x0,y1+ 20, f"{object_class_name} {confidence.item()}")
---
+++ train.py
@@ -0,0 +1,161 @@
+import argparse
+import os
+import time
+
+import matplotlib.pyplot as plt
+import torch
+from torch.optim import SGD
+from torchvision import utils
+
+from utils import create_dataloader, YOLOv1Loss, parse_cfg, build_model
+
+# from torchviz import make_dot
+
+
+def train(model, train_loader, optimizer, epoch, device, S, B, train_loss_lst, output_path):
+    model.train()  # Set the module in training mode
+    train_loss = 0
+    for batch_idx, (inputs, labels) in enumerate(train_loader):
+        t_start = time.time()
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+
+        # back prop
+        criterion = YOLOv1Loss(S, B)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        t_batch = time.time() - t_start
+
+        # show batch0 dataset
+        if batch_idx == 0 and epoch == 0:
+            fig = plt.figure()
+            inputs = inputs.cpu()  # convert to cpu
+            grid = utils.make_grid(inputs)
+            plt.imshow(grid.numpy().transpose((1, 2, 0)))
+            plt.savefig(os.path.join(output_path, 'batch0.png'))
+            # plt.show()
+            plt.close(fig)
+
+        # print loss and accuracy
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.1f}%)]  Time: {:.4f}s  Loss: {:.6f}'
+                  .format(epoch, batch_idx * len(inputs), len(train_loader.dataset),
+                          100. * batch_idx / len(train_loader), t_batch, loss.item()))
+
+    # record training loss
+    train_loss /= len(train_loader)
+    train_loss_lst.append(train_loss)
+    return train_loss_lst
+
+
+def validate(model, val_loader, device, S, B, val_loss_lst):
+    model.eval()  # Sets the module in evaluation mode
+    val_loss = 0
+    # no need to calculate gradients
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            # add one batch loss
+            criterion = YOLOv1Loss(S, B)
+            val_loss += criterion(output, target).item()
+
+    val_loss /= len(val_loader)
+    print('Val set: Average loss: {:.4f}\n'.format(val_loss))
+
+    # record validating loss
+    val_loss_lst.append(val_loss)
+    return val_loss_lst
+
+
+def test(model, test_loader, device, S, B):
+    model.eval()  # Sets the module in evaluation mode
+    test_loss = 0
+    # no need to calculate gradients
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            # add one batch loss
+            criterion = YOLOv1Loss(S, B)
+            test_loss += criterion(output, target).item()
+
+    # record testing loss
+    test_loss /= len(test_loader)
+    print('Test set: Average loss: {:.4f}'.format(test_loss))
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='YOLOv1-pytorch')
+    parser.add_argument("--cfg", "-c", default="cfg/yolov1.yaml", help="Yolov1 config file path", type=str)
+    parser.add_argument("--dataset_cfg", "-d", default="cfg/dataset.yaml", help="Dataset config file path", type=str)
+    parser.add_argument("--weights", "-w", default="", help="Pretrained model weights path", type=str)
+    parser.add_argument("--output", "-o", default="output", help="Output path", type=str)
+    parser.add_argument("--epochs", "-e", default=100, help="Training epochs", type=int)
+    parser.add_argument("--lr", "-lr", default=0.002, help="Training learning rate", type=float)
+    parser.add_argument("--batch_size", "-bs", default=32, help="Training batch size", type=int)
+    parser.add_argument("--save_freq", "-sf", default=10, help="Frequency of saving model checkpoint when training",
+                        type=int)
+    args = parser.parse_args()
+
+    cfg = parse_cfg(args.cfg)
+    dataset_cfg = parse_cfg(args.dataset_cfg)
+    img_path, label_path = dataset_cfg['images'], dataset_cfg['labels']
+    S, B, num_classes, input_size = cfg['S'], cfg['B'], cfg['num_classes'], cfg['input_size']
+
+    # create output file folder
+    start = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+    output_path = os.path.join(args.output, start)
+    os.makedirs(output_path)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # build model
+    torch.manual_seed(1)
+    model = build_model(args.weights, S, B, num_classes).to(device)
+
+    # plot model structure
+    # graph = make_dot(model(torch.rand(1, 3, args.input_size, args.input_size).cuda()),
+    #                  params=dict(model.named_parameters()))
+    # graph.render('model_structure', './', cleanup=True, format='png')
+
+    # get data loader
+    train_loader, val_loader, test_loader = create_dataloader(img_path, label_path, 0.8, 0.1, 0.1, args.batch_size,
+                                                              input_size, S, B, num_classes)
+
+    optimizer = SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0005)
+    # optimizer = Adam(model.parameters(), lr=lr)
+
+    train_loss_lst, val_loss_lst = [], []
+
+    # train epoch
+    for epoch in range(args.epochs):
+        train_loss_lst = train(model, train_loader, optimizer, epoch, device, S, B, train_loss_lst, output_path)
+        val_loss_lst = validate(model, val_loader, device, S, B, val_loss_lst)
+
+        # save model weight every save_freq epoch
+        if epoch % args.save_freq == 0 and epoch >= args.epochs / 2:
+            torch.save(model.state_dict(), os.path.join(output_path, 'epoch' + str(epoch) + '.pth'))
+
+    test(model, test_loader, device, S, B)
+
+    # save model
+    torch.save(model.state_dict(), os.path.join(output_path, 'last.pth'))
+
+    # plot loss, save params change
+    fig = plt.figure()
+    plt.plot(range(args.epochs), train_loss_lst, 'g', label='train loss')
+    plt.plot(range(args.epochs), val_loss_lst, 'k', label='val loss')
+    plt.grid(True)
+    plt.xlabel('epoch')
+    plt.ylabel('acc-loss')
+    plt.legend(loc="upper right")
+    plt.savefig(os.path.join(output_path, 'loss_curve.jpg'))
+    plt.show()
+    plt.close(fig)
